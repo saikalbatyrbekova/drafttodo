0f2d673af9296b1817310a4900dd71e85ed6ad61
saikalbatyrbekova <132566790+saikalbatyrbekova@users.noreply.github.com>
2023-12-19 21:03:29 +0600

    Update taskmanager.js

--- FinalWebAPI/scripts/taskmanager.js
+++ FinalWebAPI/scripts/taskmanager.js
@@ -1,36 +1,47 @@
+// Function to generate the URL for tasks based on the user's ID
 const tasksUrl = (userId) => `https://656db53ebcc5618d3c23cb54.mockapi.io/todo/something/user/${userId}/tasks`;
-const todoInput = document.getElementById('addTxt');
-const btn = document.getElementById('addBtn');
-const notes = document.getElementById('notes');
-const exit = document.getElementById('exit');
-const nameTodo = document.getElementById('addTitle')
 
+// Get references to HTML elements
+const todoInput = document.getElementById('addTxt'); // Get the task input element
+const btn = document.getElementById('addBtn'); // Get the add button element
+const notes = document.getElementById('notes'); // Get the container for displaying tasks
+const exit = document.getElementById('exit'); // Get the exit button element
+const nameTodo = document.getElementById('addTitle'); // Get the task title input element
+
+// Event listener for the exit button
 exit.addEventListener('click', () => {
     console.log('Exit clicked');
+    // Redirect to the sign-in page when the exit button is clicked
     window.location.href = '/signIn.html';
 });
 
-const userId = localStorage.getItem('user_id')
+// Retrieve the user ID from local storage
+const userId = localStorage.getItem('user_id');
 
+// Function to asynchronously add a new task
 const asyncTodo = async () => {
     try {
+        // Make a POST request to the API with task data
         const response = await fetch(tasksUrl(userId), {
             method: 'POST',
             headers: { 'Accept': 'application/json', 'Content-type': 'application/json' },
             body: JSON.stringify({ title: todoInput.value, done: false, NameTasks: nameTodo.value })
         });
-        return response.json();
+        return response.json(); // Return the JSON response from the API
     } catch (e) {
         console.log(e);
     }
 };
 
+// Function to delete a task
 function deleteTask(taskId) {
+    // Create a DELETE request to remove the task with the specified ID
     const deleteRequest = new XMLHttpRequest();
     deleteRequest.open('DELETE', `${(tasksUrl(userId))}/${taskId}`);
     deleteRequest.setRequestHeader('Content-type', 'application/json');
     deleteRequest.send();
 
+    // Remove the corresponding card from the UI after the task is deleted
     deleteRequest.addEventListener('load', () => {
         const deletedCard = document.getElementById(`card-${taskId}`);
         if (deletedCard) {
@@ -39,15 +50,19 @@ function deleteTask(taskId) {
     });
 }
 
+// Function to update a task
 function updateTask(taskId) {
+    // Prompt the user for the updated task text
     const updatedText = prompt('Enter updated task:');
     if (updatedText !== null) {
+        // Make a PUT request to update the task text
         fetch(`${tasksUrl(userId)}/${taskId}`, {
             method: 'PUT',
             headers: { 'Accept': 'application/json', 'Content-type': 'application/json' },
             body: JSON.stringify({ title: updatedText })
         });
 
+        // Update the task text in the corresponding card
         const updatedCard = document.getElementById(`card-${taskId}`);
         if (updatedCard) {
             updatedCard.querySelector('h4').innerText = updatedText;
@@ -55,12 +70,16 @@ function updateTask(taskId) {
     }
 }
 
+// Function to mark a task as done
 function markAsDone(taskId) {
+    // Get the card corresponding to the task
     const doneCard = document.getElementById(`card-${taskId}`);
     if (doneCard) {
         const taskText = doneCard.querySelector('h5');
+        // Toggle the text decoration between line-through and none
         taskText.style.textDecoration = taskText.style.textDecoration === 'line-through' ? 'none' : 'line-through';
 
+        // Make a PUT request to update the 'done' status of the task
         fetch(`${tasksUrl(userId)}/${taskId}`, {
             method: 'PUT',
             headers: { 'Accept': 'application/json', 'Content-type': 'application/json' },
@@ -69,42 +88,54 @@ function markAsDone(taskId) {
     }
 }
 
+// Event listener for the 'Add' button
 btn.addEventListener('click', async (event) => {
     event.preventDefault();
+    // Validate task and title input values
     if (!todoInput.value.trim() || !nameTodo.value.trim()) {
+        // Highlight input fields with red borders if they are empty
         if (!todoInput.value.trim()) {
             todoInput.style.border = '3px solid red';
         }
         if (!nameTodo.value.trim()) {
             nameTodo.style.border = '3px solid red';
         }
-        return;
+        return; // Stop further execution if any field is empty
     }
+    // Reset borders to the default style
     todoInput.style.border = '2px solid #ced4da';
     nameTodo.style.border = '2px solid #ced4da';
+    // Add the task asynchronously and display an alert on success
     const data = await asyncTodo();
-    alert('Успешно');
+    alert('Task added successfully!');
     todoInput.value = '';
     nameTodo.value = '';
-    createCard(data);
+    createCard(data); // Create a card for the added task
 });
 
+// Event listener for when the page is fully loaded
 document.addEventListener('DOMContentLoaded', async () => {
+    // Create a GET request to fetch tasks for the user
     const request = new XMLHttpRequest();
     request.open('GET', tasksUrl(userId));
     request.setRequestHeader('Content-type', 'application/json');
     request.send();
 
+    // Add an event listener to handle the response when it's loaded
     request.addEventListener('load', () => {
+        // Parse the JSON response from the API
         const data = JSON.parse(request.response);
-        data.forEach(createCard);
+        data.forEach(createCard); // Create a card for each task
     });
 });
 
+// Function to create a card for a task
 function createCard(dataTitle) {
+    // Create a new card element
     const card = document.createElement('div');
     card.className = 'card-info';
     card.id = `card-${dataTitle.id}`;
+    // Set the inner HTML of the card with task information
     card.innerHTML = `
         <h5 style="text-decoration: ${dataTitle.done ? 'line-through' : 'none'}">${dataTitle.title}</h5>
         <p>${dataTitle.NameTasks}</p>
@@ -112,39 +143,28 @@ function createCard(dataTitle) {
         <button class="change Button">Update</button>
         <button class="wins Button">Done</button>
     `;
-    notes.append(card);
+    notes.append(card); // Append the card to the container
 }
 
+// Event listener for clicks on the notes container
 notes.addEventListener('click', (event) => {
+    // Handle different button clicks based on the class of the clicked element
     if (event.target.classList.contains('delete')) {
         const taskId = event.target.parentElement.id.split('-')[1];
-        deleteTask(taskId);
+        deleteTask(taskId); // Delete the task when the 'Delete' button is clicked
     } else if (event.target.classList.contains('change')) {
         const taskId = event.target.parentElement.id.split('-')[1];
-        updateTask(taskId);
+        updateTask(taskId); // Update the task when the 'Update' button is clicked
     } else if (event.target.classList.contains('wins')) {
         const taskId = event.target.parentElement.id.split('-')[1];
-        markAsDone(taskId);
+        markAsDone(taskId); // Mark the task as done when the 'Done' button is clicked
     }
 });
 
+// Get reference to the filter dropdown element
 const filterSelect = document.getElementById('filter');
 
+// Event listener for changes in the filter dropdown
 filterSelect.addEventListener('change', () => {
-    const filterValue = filterSelect.value.toLowerCase();
-    const cards = document.querySelectorAll('.card-info');
-
-    cards.forEach((card) => {
-        const isCompleted = card.querySelector('h5').style.textDecoration === 'line-through';
-
-        if (
-            (filterValue === 'all') ||
-            (filterValue === 'completed' && isCompleted) ||
-            (filterValue === 'incomplete' && !isCompleted)
-        ) {
-            card.style.display = 'block';
-        } else {
-            card.style.display = 'none';
-        }
-    });
-});
+    // Get the selected filter value
+    const filterValue = filterSelect
