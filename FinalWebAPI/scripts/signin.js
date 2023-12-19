80ba19cfbe551ef63b3e45605293c33ddc272a12
saikalbatyrbekova <132566790+saikalbatyrbekova@users.noreply.github.com>
2023-12-19 21:01:45 +0600

    Update signin.js

--- FinalWebAPI/scripts/signin.js
+++ FinalWebAPI/scripts/signin.js
@@ -1,62 +1,62 @@
-const signInForm = document.getElementById('newSignUpForm')
-const userNameInput = document.getElementById('newUserNameInput')
-const emailInput = document.getElementById('newEmailInput')
-const passwordInput = document.getElementById('newPasswordInput')
+// Get references to HTML elements by their IDs
+const signUpForm = document.getElementById('signUpForm'); // Get the form element
+const userNameInput = document.getElementById('userNameInput'); // Get the username input element
+const emailInput = document.getElementById('emailInput'); // Get the email input element
+const passwordInput = document.getElementById('passwordInput'); // Get the password input element
 
-const BASE_URL_two = 'https://656db53ebcc5618d3c23cb54.mockapi.io/todo/something/user'
+// Define the URL for the API and the next page to go to after signing up
+const nextPage = '/signIn.html';
+const BASE_URL = 'https://658194e23dfdd1b11c43a72e.mockapi.io/user';
 
-
-
-
-
-
-signInForm.addEventListener('submit', (event) => {
-    event.preventDefault()
-
-
-
-    userNameInput.classList.remove('error-border')
-    emailInput.classList.remove('error-border')
-    passwordInput.classList.remove('error-border')
-
-    if (!userNameInput.value.trim() || !emailInput.value.trim() || !passwordInput.value.trim()) {
-        if (!userNameInput.value.trim()) {
-            userNameInput.classList.add('error-border')
-        }
-        if (!emailInput.value.trim()) {
-            emailInput.classList.add('error-border')
-        }
-        if (!passwordInput.value.trim()) {
-            passwordInput.classList.add('error-border')
-        }
-        return
+// Function to validate the form inputs
+const validate = () => {
+    // Check if all input fields have values
+    if (userNameInput.value && emailInput.value && passwordInput.value) {
+        // If all fields have values, the form is valid
+        return true;
+    } else {
+        // If any field is empty, highlight the error by adding an 'error' class
+        !userNameInput.value ? userNameInput.classList.add('error') : userNameInput.classList.remove('error');
+        !emailInput.value ? emailInput.classList.add('error') : emailInput.classList.remove('error');
+        !passwordInput.value ? passwordInput.classList.add('error') : passwordInput.classList.remove('error');
     }
+}
 
-    const request = new XMLHttpRequest()
-    request.open('GET', `${BASE_URL_two}`)
-    request.setRequestHeader('Content-type', 'application/json')
-    request.send()
-    request.addEventListener('load', () => {
-        const data = JSON.parse(request.response)
-        let found = false;
+// Function to asynchronously sign up the user
+const asyncSignUp = async () => {
+    try {
+        // Make a POST request to the API with user data
+        const response = await fetch(`${BASE_URL}`, {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            // Send user data as JSON in the request body
+            body: JSON.stringify({
+                username: userNameInput.value,
+                email: emailInput.value,
+                password: passwordInput.value,
+            })
+        });
+        // Return the JSON response from the API
+        return response.json();
+    }
+    catch (e) {
+        // Log any errors that occur during the API request
+        console.log('Error:', e);
+    }
+}
 
-        for (let i = 0; i < data.length; i++) {
-            const dataInfo = data[i]
-            if (
-                userNameInput.value === dataInfo.username &&
-                emailInput.value === dataInfo.email &&
-                passwordInput.value === dataInfo.password
-            ) {
-                found = true;
-                window.location.href = '/taskmanager.html'
-                localStorage.setItem('user_id', dataInfo.id)
-            }
-        }
+// Event listener for form submission
+signUpForm.onsubmit = event => {
+    // Prevent the default form submission behavior
+    event.preventDefault();
 
-        if (!found) {
-            userNameInput.classList.add('error-border')
-            emailInput.classList.add('error-border')
-            passwordInput.classList.add('error-border')
-        }
-    })
-})
+    // Validate the form inputs
+    validate()
+        // If the form is valid, call the asyncSignUp function and redirect to the next page
+        ? asyncSignUp().then(() => window.location.href = nextPage)
+        // If the form is not valid, show an alert indicating an error in sign-up
+        : alert('Error in SignUp! Please fill out all fields.');
+}
